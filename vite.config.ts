24ffc6790896018c4387d6e3f7ee54874898b5f2
Gill <gillarete@gmail.com>
2025-05-16 15:45:38 +0800

    Fix the 'waiting for the frontend server' issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import path from 'path';
 
-const host = process.env.TAURI_DEV_HOST;
+const host = process.env.TAURI_PLATFORM === 'windows' ? 'localhost' : '0.0.0.0';
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -13,18 +13,18 @@ export default defineConfig({
 		host: host || false,
 		hmr: host
 			? {
-				protocol: "ws",
-				host,
-				port: 1421,
-			}
+					protocol: 'ws',
+					host,
+					port: 1421
+				}
 			: undefined,
 		watch: {
-			ignored: ["**/src-tauri/**"],
-		},
+			ignored: ['**/src-tauri/**']
+		}
 	},
 	resolve: {
 		alias: {
-			$lib: path.resolve("./src/lib"),
-		},
-	},
+			$lib: path.resolve('./src/lib')
+		}
+	}
 });
